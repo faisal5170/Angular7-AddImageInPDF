a19cce7e67329d4edd14f082c78e0ef6b362833d
Faisal Pathan <faisalmpathan@gmail.com>
2019-06-18 12:32:00 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular7-AddImageInPDF';
+  fileName: string;
+  filePreview: string
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  onFileChanged(event: { target: { files: any[]; }; }) {
+
+    let reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.fileName = file.name + " " + file.type;
+        const doc = new jsPDF();
+        const base64ImgString = (reader.result as string).split(',')[1];
+        doc.addImage(base64ImgString, 15, 40, 50, 50);
+        this.filePreview = 'data:image/png' + ';base64,' + base64ImgString;
+        doc.save('TestPDF')
+      };
+    }
+  }
+
+  sanitize(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
 }
